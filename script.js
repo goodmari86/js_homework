d70f6481e970f8c558eeda1c30b1623636b0f1c0
Mariia <goodmari86@gmail.com>
2024-02-26 23:10:23 +0200

    homework2

---
+++ script.js
@@ -0,0 +1,24 @@
+var age = Number(prompt('Ваш вік', '18'));
+var txt = 'років';
+if(typeof age === "number" && age > 0 && age <= 100){
+    
+    if(age == 1 || age == 21  || age == 31 || age == 41 || age == 51 || age == 61 || age == 71 || age == 81 || age == 91){
+        var txt = 'рік';   
+    }
+    if(age == 2 || age == 3  || age == 4 || 
+        age == 22 || age == 23 || age == 24 || 
+        age == 32 || age == 33 || age == 34 ||
+        age == 42 || age == 43 || age == 44 ||
+        age == 52 || age == 53 || age == 54 ||
+        age == 62 || age == 63 || age == 64 ||
+        age == 72 || age == 73 || age == 74 ||
+        age == 82 || age == 83 || age == 84 ||
+        age == 92 || age == 93 || age == 94){
+        var txt = 'роки';   
+    }
+    
+    console.log(age+' '+txt);
+}else{
+    console.log("Wrong input data");
+}
+
