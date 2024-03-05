7861f683ec5c770d2ab6db1a5abd914548a9b2a3
Mariia <goodmari86@gmail.com>
2024-03-05 21:44:54 +0200

    Dz 6 Work with cycle

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-/*var age = Number(prompt('Ваш вік', '18'));
-var txt = 'років';
-if(typeof age === "number" && age > 0 && age <= 100){
-    
-    if(age == 1 || age == 21  || age == 31 || age == 41 || age == 51 || age == 61 || age == 71 || age == 81 || age == 91){
-        var txt = 'рік';   
-    }
-    if(age == 2 || age == 3  || age == 4 || 
-        age == 22 || age == 23 || age == 24 || 
-        age == 32 || age == 33 || age == 34 ||
-        age == 42 || age == 43 || age == 44 ||
-        age == 52 || age == 53 || age == 54 ||
-        age == 62 || age == 63 || age == 64 ||
-        age == 72 || age == 73 || age == 74 ||
-        age == 82 || age == 83 || age == 84 ||
-        age == 92 || age == 93 || age == 94){
-        var txt = 'роки';   
-    }
-    
-    console.log(age+' '+txt);
-}else{
-    console.log("Wrong input data");
-}*/
 
-var type = 'km';
-var value = 10;
-switch (type) {
-    case 'km':
-        console.log(value+' km = ' + (value*1000)+' meters');
-        break
-    case 'kg':
-        console.log(value+' kg = ' + (value*1000)+' gramms');
-        break;
-    case 'hour':
-        console.log(value+' hour = ' + (value*60)+ ' minutes');
-        break;
-    default:
-        console.log('Undefined type');
+function drawTriangle(lineCount, symbol) {
+    
+    for (let i = 1; i <= lineCount; i++) {
+        let line = '';
+        for (let j = 1; j <= i; j++) {
+            line = line + symbol;
+        }
+        console.log(line);
+    }
 }
 
+drawTriangle(10,'+');
+
+
+function calcSum(from, to) {
+    let i = from;
+    let result = 0;
+    while (i <= to) {
+        if(i % 3 === 0){
+            result = result + i;
+        }
+       i++;
+    }
+    console.log(result);
+}
+calcSum(1, 100);
+
+
+function pow(x, y) {
+    let i = 2;
+    let result = x;
+    while (i <= y) {
+       result = result * x;
+       i++;
+    }
+    console.log(result);
+}
+
+pow(2,3);
